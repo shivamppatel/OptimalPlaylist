8a38551445fa5391bffebb7959484cd6363377fc
Shivam <patel1.shivam2@gmail.com>
2018-06-10 12:57:01 -0400

    demo

--- application.py
+++ application.py
@@ -33,7 +33,7 @@ SPOTIFY_API_URL = "{}/{}".format(SPOTIFY_API_BASE_URL, API_VERSION)
 # Server-side Parameters
 CLIENT_SIDE_URL = "http://127.0.0.1"
 PORT = 4000
-REDIRECT_URI = "http://127.0.0.1:8080/callback/q"
+REDIRECT_URI = "https://optimalplaylist.herokuapp.com/callback/q"
 SCOPE = "playlist-modify-public playlist-modify-private"
 STATE = ""
 SHOW_DIALOG_bool = True
